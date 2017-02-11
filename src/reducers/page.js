40f3102089763c6665010df2dd7d489bec55fae7
cyxn <suhovss123@gmail.com>
2017-02-11 10:54:17 +0200

    added saving into local storage

--- src/reducers/page.js
+++ src/reducers/page.js
@@ -3,22 +3,27 @@ import {
 } from '../constants/ActionTypes';
 
 const initialState = {
-  favoritesList: []
+  favoritesList: JSON.parse(localStorage.getItem('favoritesList')) || []
 }
 
 export default function page(state = initialState, action) {
   switch (action.type) {
     case ADD_FAVORITES:
-      return {...state, favoritesList: [...state.favoritesList, action.id]}
+      const newFavList = [...state.favoritesList, action.id];
+      localStorage.setItem('favoritesList', JSON.stringify(newFavList));
+      return {...state, favoritesList: newFavList};
     case REMOVE_FAVORITES:
       const index = state.favoritesList.indexOf(action.id);
+      const newFavList2 = [
+        ...state.favoritesList.slice(0, index),
+        ...state.favoritesList.slice(index + 1)
+      ];
+      localStorage.setItem('favoritesList', JSON.stringify(newFavList2));
       return {
         ...state,
-        favoritesList: [
-          ...state.favoritesList.slice(0, index),
-          ...state.favoritesList.slice(index + 1)
-        ]
+        favoritesList: newFavList2
       }
+
     default:
       return state;
   }
